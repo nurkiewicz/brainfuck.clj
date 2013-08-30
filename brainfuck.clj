25aec2a06ea0842270f1a5afcf39e4c501505ea5
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2013-08-30 15:56:21 +0200

    Getting rid of duplicated code

--- brainfuck.clj
+++ brainfuck.clj
@@ -1,5 +1,11 @@
 (defn brainfuck-interpreter [& lines]
-	(let [program (str lines)]
+	(let [program (str lines)
+		goto-bracket (fn [same-bracket other-bracket inst-ptr dir] 
+			(loop [i (dir inst-ptr) opened 0]
+				(condp = (nth program i)
+					same-bracket	(recur (dir i) (inc opened))
+					other-bracket	(if (zero? opened) i (recur (dir i) (dec opened)))
+					(recur (dir i) opened))))]
 		(loop [cells [0N], cells-ptr 0, inst-ptr 0]
 			(condp = (get program inst-ptr)
 				\>	(let [next-ptr (inc cells-ptr)
@@ -13,23 +19,9 @@
 						(recur cells cells-ptr (inc inst-ptr)))
 				\,	(let [ch (.read System/in)]
 						(recur (assoc cells cells-ptr ch) cells-ptr (inc inst-ptr)))
-				\[	(if (zero? (nth cells cells-ptr))
-						(recur cells cells-ptr
-							(loop [i (inc inst-ptr) opened 0]
-								(condp = (nth program i)
-									\[	(recur (inc i) (inc opened))
-									\]	(if (zero? opened)
-											(inc i)
-											(recur (inc i) (dec opened)))
-									(recur (inc i) opened))))
-						(recur cells cells-ptr (inc inst-ptr)))
-			\]	(recur cells cells-ptr
-					(loop [i (dec inst-ptr) opened 0]
-						(condp = (nth program i)
-							\]	(recur (dec i) (inc opened))
-							\[	(if (zero? opened)
-									i
-									(recur (dec i) (dec opened)))
-							(recur (dec i) opened))))
+				\[	(recur cells cells-ptr (inc (if (zero? (nth cells cells-ptr))
+						(goto-bracket \[ \] inst-ptr inc)
+						inst-ptr)))
+				\]	(recur cells cells-ptr (goto-bracket \] \[ inst-ptr dec))
 				nil cells
 				(recur cells cells-ptr (inc inst-ptr))))))
